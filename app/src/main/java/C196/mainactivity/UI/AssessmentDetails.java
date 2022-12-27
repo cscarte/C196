aab79ee72bfba3ca61314b4753daacb4ca0408ec
cscarte <cscarter2015@gmail.com>
2022-12-27 14:50:04 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_assessments_details.xml

--- app/src/main/java/C196/mainactivity/UI/AssessmentDetails.java
+++ app/src/main/java/C196/mainactivity/UI/AssessmentDetails.java
@@ -2,22 +2,38 @@ package C196.mainactivity.UI;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import C196.mainactivity.R;
 
 public class AssessmentDetails extends AppCompatActivity {
+    TextView assessmentID;
     EditText assessmentTitle;
     EditText assessmentStatus;
+    EditText assessmentDueDate;
+    EditText assessmentGoalDate;
+    CheckBox assessmentGoalDateAlert;
+    Switch assessmentObjective;
+    TextView courseID;
+
 
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessments_details);
 
-        assessmentTitle = findViewById(R.id.editTextAssessmentTitle);
-        assessmentStatus = findViewById()
+        assessmentID = findViewById(R.id.assessmentIDLabelDisplay);
+        assessmentTitle = findViewById(R.id.editAssessmentTitle);
+        assessmentStatus = findViewById(R.id.editAssessmentStatus);
+        assessmentDueDate = findViewById(R.id.editAssessmentDueDate);
+        assessmentGoalDate = findViewById(R.id.editAssessmentGoalDate);
+        assessmentGoalDateAlert = findViewById(R.id.editGoalDateAlert);
+        assessmentObjective = findViewById(R.id.editAssessmentObjective);
+        courseID = findViewById(R.id.courseIDLabelDisplay);
     }
 }
