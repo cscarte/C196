b847d57aad26473a46c3e95b9e138bcb6e197f2d
ccarter1990 <ccar491@wgu.edu>
2023-01-31 19:28:31 -0600

    01/31/23

--- app/src/main/java/C196/mainactivity/UI/CourseDetails.java
+++ app/src/main/java/C196/mainactivity/UI/CourseDetails.java
@@ -1,33 +1,58 @@
 package C196.mainactivity.UI;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
+import C196.mainactivity.Database.Repository;
+import C196.mainactivity.Entity.Course;
 import C196.mainactivity.R;
 
 public class CourseDetails extends AppCompatActivity {
     EditText courseName;
-    EditText courseStatus;
-    EditText courseStartDate;
+    TextView courseStatus;
+    TextView courseStartDate;
     CheckBox courseStartDateAlert;
-    EditText courseEndDate;
+    TextView courseEndDate;
     CheckBox courseEndDateAlert;
     EditText courseNotes;
+    EditText courseInstructorName;
+    EditText courseInstructorPhone;
+    EditText courseInstructorEmail;
+
     Button  courseShareNotesButton;
     Button courseSaveButton;
+
+
+    private DatePickerDialog.OnDateSetListener startDateListener;
+    private DatePickerDialog.OnDateSetListener endDateListener;
 
     String name;
     String status;
     String startDate;
     String endDate;
     String notes;
+    String instructorName;
+    String instructorPhone;
+    String instructorEmail;
 
+    int courseID;
+    int assessmentID;
+    Course course;
 
     @Override
     public ComponentName getComponentName() {
@@ -38,6 +63,9 @@ public class CourseDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses_details);
+
+        courseID = getIntent().getIntExtra("courseID", -1);
+        assessmentID = getIntent().getIntExtra("assessmentID", -1);
 
         courseName = findViewById(R.id.courseDetailsCourseTitle);
         name = getIntent().getStringExtra("courseName");
@@ -50,13 +78,104 @@ public class CourseDetails extends AppCompatActivity {
         courseStartDate = findViewById(R.id.courseDetailsCourseStartDate);
         startDate = getIntent().getStringExtra("courseStartDate");
         courseStartDate.setText(startDate);
+        courseStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CourseDetails.this, android.R.style.Theme_Holo_Light_Dialog, startDateListener, year, month, day);
+
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        startDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+
+                String textViewDueDate = month + "/" + day + "/" + year;
+                courseStartDate.setText(textViewDueDate);
+            }
+        };
 
         courseEndDate = findViewById(R.id.courseDetailsCourseEndDate);
         endDate = getIntent().getStringExtra("courseEndDate");
         courseEndDate.setText(endDate);
+        courseEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CourseDetails.this, android.R.style.Theme_Holo_Light_Dialog, endDateListener, year, month, day);
+
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        endDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+
+                String textViewDueDate = month + "/" + day + "/" + year;
+                courseEndDate.setText(textViewDueDate);
+            }
+        };
+
 
         courseNotes = findViewById(R.id.courseDetailsCourseNotesMultiLineText);
         notes = getIntent().getStringExtra("courseShareNotes");
         courseNotes.setText(notes);
+
+
+        courseInstructorName = findViewById(R.id.editTextCourseInstructorName);
+        instructorName = getIntent().getStringExtra("courseInstructorName");
+        courseInstructorName.setText(instructorName);
+
+        courseInstructorPhone = findViewById(R.id.editTextCourseInstructorPhone);
+        instructorPhone = getIntent().getStringExtra("courseInstructorPhone");
+        courseInstructorPhone.setText(instructorPhone);
+
+        courseInstructorEmail = findViewById(R.id.editTextCourseInstructorEmail);
+        instructorEmail = getIntent().getStringExtra("courseInstructorEmail");
+        courseInstructorEmail.setText(instructorEmail);
+
+
+        Repository repository = new Repository(getApplication());
+
+        courseSaveButton = findViewById(R.id.courseDetailsSaveButton);
+        // Add drop down menu for course status
+        courseSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (courseID == -1){
+                    course = new Course(0, courseName.getText().toString(), courseStatus.getText().toString(), courseStartDate.getText().toString(), courseEndDate.getText().toString(), courseNotes.getText().toString(), courseInstructorName.getText().toString(), courseInstructorPhone.getText().toString(), courseInstructorEmail.getText().toString());
+                    repository.insert(course);
+
+                    onBackPressed();
+                } else {
+                    course = new Course(courseID, courseName.getText().toString(), courseStatus.getText().toString(), courseStartDate.getText().toString(), courseEndDate.getText().toString(), courseNotes.getText().toString(), courseInstructorName.getText().toString(), courseInstructorPhone.getText().toString(), courseInstructorEmail.getText().toString());
+                    repository.update(course);
+
+                    onBackPressed();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(this, CoursesList.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
